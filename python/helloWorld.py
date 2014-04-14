9621610c701d4530475ce140ab90c11555d71299
jot.zhao@gmail.com <jot.zhao@gmail.com>
2014-04-14 22:55:18 +0800

    python socket

--- python/helloWorld.py
+++ python/helloWorld.py
@@ -1,5 +1,6 @@
 #coding=utf-8
 import os
+import socket
 
 def helloWorld(name):
 	print "Hello World,",name
@@ -13,7 +14,29 @@ def helloWorldWithFile():
 	file = open(filepath,'r')
 	print "Hello World,",file.readline()
 
+def helloWorldWithNetWork():
+	sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+	sock.bind(('localhost',8001))
+	sock.listen(5)
+	while True:
+		connection,address = sock.accept()
+		try:
+			connection.settimeout(5)
+			buf = connection.recv(1024)
+			print buf
+			if buf == 'welcome':
+				connection.send("welcome to server")
+			elif buf == 'quit':
+				connection.close()
+			else:
+				connection.send('please go out')
+			
+		except socket.timeout:
+			print 'time out'
+		
+
 if __name__ == '__main__':
 	helloWorld('jotZhao')
 	helloWorldWithInput()
 	helloWorldWithFile()
+	helloWorldWithNetWork()
